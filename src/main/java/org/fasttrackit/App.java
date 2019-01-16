6e63932963e79101841a8925035d2f5151801d40
anamariahalasz <ana.mariahalasz@yahoo.com>
2019-01-16 16:04:06 +0200

    calculations - Method Overloading

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -11,5 +11,9 @@ public class App
         System.out.println("add: " + calculations.add(2.54, 1.31));
         System.out.println("substract: " + calculations.substract(7.64, 4.38));
         System.out.println("multiply: " + calculations.multiply(2.75, 3.97));
+
+        System.out.println("add: " + calculations.add(32, 13));
+        System.out.println("substract: " + calculations.substract(45, 12));
+        System.out.println("multiply: " + calculations.multiply(21, 48));
     }
 }
--- src/main/java/org/fasttrackit/Calculations.java
+++ src/main/java/org/fasttrackit/Calculations.java
@@ -14,4 +14,16 @@ public class Calculations {
         return firstNumber * secondNumber;
     }
 
+    public int add (int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public int substract (int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiply (int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
 }
