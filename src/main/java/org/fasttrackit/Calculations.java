bf97653be6f6ac53b07445527df584a49285484c
anamariahalasz <ana.mariahalasz@yahoo.com>
2019-01-16 15:47:40 +0200

    calculations

---
+++ src/main/java/org/fasttrackit/Calculations.java
@@ -0,0 +1,17 @@
+package org.fasttrackit;
+
+public class Calculations {
+
+    public double add (double firstNumber, double secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public double substract (double firstNumber, double secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public double multiply (double firstNumber, double secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+}
